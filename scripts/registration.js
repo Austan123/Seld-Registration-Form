e0bcfa92ba56e4a9871be34820badf032914737b
izzy417 <israeldail2@gmail.com>
2023-03-26 22:13:01 +0000

    posting to google sheets works! needs minor changes to work completely.

--- scripts/registration.js
+++ scripts/registration.js
@@ -30,11 +30,25 @@ virtualNode.addEventListener("click", (e) => {
 })
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    const formData = new  FormData(e.target);
+    const formData = new FormData(e.target);
     const data = [...formData.entries()]
-       
+
     console.log(data);
 })
 
 
-
+const msg = document.getElementById("msg")
+const scriptURL = "https://script.google.com/macros/s/AKfycbyW2_2diylogBRguVJX5QDAbYMDmA45C9gqL3IjTInAxXg_wEVWSZlODpcvG5wJIJQm/exec";
+const form_reg = document.forms["submit-to-google-sheet"];
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, {method: "POST", body: new FormData(form_reg)})
+        .then(response => {
+            msg.innerHTML = "Thank you for your submission!"
+            setTimeout(function() {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset();
+        })
+        .catch(error => console.error("Error!", error.message))
+})
